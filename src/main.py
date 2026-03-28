6bab69d8e976bf97db22a60ec530521b5c24b343
SallahAhmed <SallahAhmeed77@gmail.com>
2026-03-28 05:37:11 +0200

    update all files

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-import fastapi
+from fastapi import FastAPI
+from Routes import base, data
 from dotenv import load_dotenv
 load_dotenv(".env")
-
-from Routes import base
-app = fastapi.FastAPI()
+app = FastAPI()
 
 app.include_router(base.base_router)
+app.include_router(data.data_router)
+
