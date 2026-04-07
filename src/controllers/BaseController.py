f15a3570181283358f5cf162d69c0e00b666bca3
SallahAhmed <SallahAhmeed77@gmail.com>
2026-04-07 14:03:59 +0200

    feat: implement Qdrant vector database provider and integrate into FastAPI application lifecycle

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -15,7 +15,23 @@ class BaseController:
             self.base_dir,
             "assets/files"
         )
+
+        self.vector_db_path = os.path.join(
+            self.base_dir,
+            "assets/vector_db"
+        )
         
     def generate_random_string(self, length: int=12):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
-        
+
+
+    def get_database_path(self, db_name: str):
+
+        database_path = os.path.join(
+            self.vector_db_path, db_name
+        )
+
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+
+        return database_path
